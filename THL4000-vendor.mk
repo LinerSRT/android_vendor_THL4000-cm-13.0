79483f810fba8f5f4602a991d66bf6b197aed825
Yuri <SeriniTY320@gmail.com>
2016-11-06 19:59:33 +0300

    Add files via upload

---
+++ THL4000-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Technology-Happy-Life/THL4000/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Technology-Happy-Life/THL4000/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Technology-Happy-Life/THL4000/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Technology-Happy-Life/THL4000/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Technology-Happy-Life/THL4000/xbin,system/xbin)
+
